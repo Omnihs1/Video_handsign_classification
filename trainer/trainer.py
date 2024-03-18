0abe98099d2634198e5d3bd8063ea8fb1530c521
Omnihs1 <antandbee89@gmail.com>
2024-03-18 10:14:14 +0700

    init

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -2,6 +2,9 @@ import torch
 from torch import nn
 import pytorch_lightning as pl
 import torch.optim as optim
+from torchmetrics.classification import MulticlassAccuracy
+import numpy as np
+from tqdm import tqdm 
 
 class ViVitTrainer():
     def __init__(self, model, weight_decay, lr, epochs):
@@ -13,8 +16,10 @@ class ViVitTrainer():
     def forward(self, x):
         return self.model(x)
 
-    def fit(self, train_dataloader, test_dataloader, device, epochs = 1):
+    def fit(self, train_dataloader, test_dataloader, device):
         self.model.train(True)
+        train_loss = []
+        test_loss = []
         for i in range(self.epochs):
             avg_loss = self.train_one_epoch(train_dataloader, device)
             running_vloss = 0.0
@@ -23,18 +28,31 @@ class ViVitTrainer():
             self.model.eval()
             # Disable gradient computation and reduce memory consumption.
             # running_vloss = 0.
+            acc = 0
             with torch.no_grad():
                 for i, vdata in enumerate(test_dataloader):
                     vinputs, vlabels = vdata
                     vinputs = vinputs.to(device)
                     vlabels = vlabels.to(device)
                     voutputs = self.model(vinputs)
+                    metric = MulticlassAccuracy(num_classes=8).to(device)
+                    acc = metric(voutputs, vlabels)
                     vloss = self.loss_fn(voutputs, vlabels)
                     running_vloss += vloss
 
-        # avg_vloss = running_vloss / (i + 1)
-        print('LOSS train {} valid {}'.format(avg_loss, running_vloss))
+            # avg_vloss = running_vloss / (i + 1)
+            print('LOSS train {} valid {}'.format(avg_loss, running_vloss))
+            print('ACC valid {}'.format(acc))
+            # Sample list of numbers
+            train_loss.append(avg_loss.cpu())
+            test_loss.append(running_vloss.cpu())
         # print(f"Loss {running_vloss}")
+        # Convert the list to a NumPy array
+        train_loss = np.array(train_loss)
+        test_loss = np.array(test_loss)
+        # Save the array to a .npy file
+        np.save('train_loss.npy', train_loss)
+        np.save('test_loss.npy', test_loss)
         torch.save(self.model.state_dict(), "model.pt")
 
     def train_one_epoch(self, dataloader, device):
@@ -43,7 +61,7 @@ class ViVitTrainer():
         # Here, we use enumerate(training_loader) instead of
         # iter(training_loader) so that we can track the batch
         # index and do some intra-epoch reporting
-        for i, data in enumerate(dataloader):
+        for i, data in tqdm(enumerate(dataloader)):
             # Every data instance is an input + label pair
             inputs, labels = data
             inputs = inputs.to(device)
